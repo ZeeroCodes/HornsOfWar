6ffce64523cb809d9c18f92380dc2afd0064a78e
Zero <129365984+ZeeroCodes@users.noreply.github.com>
2023-10-16 20:37:52 +0200

    V00.01.02 - Fixed bug with surrounded units

--- Version.py
+++ Version.py
@@ -1,7 +1,7 @@
 class Version(object):
 
     def __init__(self, rows = 0, cols = 0, tiles_dictionary = None):
-        self.version = "00.01.01"
+        self.version = "00.01.02"
 
     def get_version(self):
         return self.version
